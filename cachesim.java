b8ad649488f28f42da8e87e2026ef5ac6e722cda
Jay Yoon <jiwon.yoon@duke.edu>
2022-04-13 21:51:53 -0400

    store in progress

--- cachesim.java
+++ cachesim.java
@@ -11,7 +11,7 @@ public class cachesim {
     public static int indexSize = 0;
     public static int offsetSize = 0;
     public static int time = 0;
-
+    public static boolean writeBack;
     public static int addressSize = 16;
 
     public static cacheSet[] cache;
@@ -47,6 +47,12 @@ public class cachesim {
         indexSize = (int) (Math.log(setNum)/Math.log(2));
         offsetSize = (int) (Math.log(blockSize)/Math.log(2));
         tagSize = addressSize - indexSize - offsetSize;
+        if (args[3].equals("wb")) {
+            writeBack = true;
+        }
+        else {
+            writeBack = false;
+        }
         setupCache();
         readInstructions(scnr);
     }
@@ -71,11 +77,13 @@ public class cachesim {
             String operation = arr[0];
             int accessBytes = Integer.parseInt(arr[2]);
             if (operation.equals("load")) {
-                String result = requestLoad(hexAddress, accessBytes);
-                printResult(operation, hexAddress, result);
+                String loadResult = requestLoad(hexAddress, accessBytes);
+                printResult(operation, hexAddress, loadResult);
             }
             if (operation.equals("store")) {
-                System.out.println("store instruction detected");
+                String storeData = arr[3];
+                String storeResult = requestStore(hexAddress, accessBytes, storeData);
+                printResult(operation, hexAddress, storeResult);
             }
             time++;
         }
@@ -87,7 +95,7 @@ public class cachesim {
     }
 
     public static String requestLoad(String address, int bytes) {
-        String missHit = "";
+        String missOrHit = "";
         String add = hexToBinary(address);
         while(add.length()<16) {
             add = "0"+add;
@@ -96,8 +104,70 @@ public class cachesim {
         int index = Integer.parseInt(add.substring(add.length()-offsetSize-indexSize, add.length()-offsetSize), 2);
         String tag = add.substring(0, add.length()-offsetSize-indexSize);
   
-        missHit += loadData(add, bytes, blockOffset, index, tag);
-        return missHit;
+        missOrHit += loadData(add, bytes, blockOffset, index, tag);
+        return missOrHit;
+    }
+
+    public static String requestStore(String address, int bytes, String storeData) {
+        String missOrHit = "";
+        String add = hexToBinary(address);
+        while(add.length()<16) {
+            add = "0"+add;
+        }
+        int blockOffset = Integer.parseInt(add.substring(add.length()-offsetSize), 2);
+        int index = Integer.parseInt(add.substring(add.length()-offsetSize-indexSize, add.length()-offsetSize), 2);
+        String tag = add.substring(0, add.length()-offsetSize-indexSize);
+  
+        missOrHit += storeData(add, bytes, blockOffset, index, tag, storeData);
+        return missOrHit;
+    }
+
+    public static String storeData(String add, int bytes, int blockOffset, int index, String tag, String storeData) {   
+        //just implementing write through, non-allocate...
+        boolean inCache = false; 
+        for (int i=0; i<cache[index].blocks.size(); i++) {
+            if(((cache[index].blocks.get(i).tag).equals(tag))&&((cache[index].blocks.get(i).validBit)==1)) {
+                String newInfo = cache[index].blocks.get(i).data.substring(0, blockOffset);
+                newInfo += storeData;
+                newInfo += cache[index].blocks.get(i).data.substring(blockOffset+storeData.length());
+                cache[index].blocks.get(i).data = newInfo;
+                cache[index].blocks.get(i).counter = time;
+                if (writeBack) {
+                    cache[index].blocks.get(i).dirtyBit = 1;
+                }
+                inCache = true;
+            }
+        }
+        writeToMemory(add, bytes, blockOffset, index, tag, storeData);
+        
+    /* int decAddress = Integer.parseInt(binaryToDecimal(add));
+        for (int j=0; j<bytes; j++) {
+            newInfo += mainMemory[decAddress+j];
+        }
+        int startPoint = (decAddress / blockSize)*blockSize ;
+        String blockInfo = "";
+        for (int k=0; k<blockSize; k++) {
+            blockInfo += mainMemory[startPoint+k];
+        }
+        Block b = new Block(tag, 1, 0, blockInfo, time);
+        if (cache[index].blocks.size()>=assoc) {
+            int LRUindex = removeLRU(cache[index].blocks);
+            cache[index].blocks.remove(LRUindex);
+        }
+        cache[index].blocks.add(b);
+    */
+        if (inCache) { 
+            return "hit";
+        }
+        return "miss";
+    }
+
+    public static void writeToMemory(String add, int bytes, int blockOffset, int index, String tag, String storeData) {
+        int decAddress = Integer.parseInt(binaryToDecimal(add));
+        for (int i=0; i<bytes; i++) {
+            String byteStore = storeData.substring(i*2, i*2+2);
+            mainMemory[decAddress+i] = byteStore;
+        }
     }
 
     public static String loadData(String add, int bytes, int blockOffset, int index, String tag) {
